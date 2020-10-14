2f00a3200ce193d7f6810cc7566f1b7f7e8c6c72
Brian Park <park-brian@users.noreply.github.com>
2020-10-14 11:32:26 -0400

    use dirname instead of pwd

--- util.py
+++ util.py
@@ -18,7 +18,7 @@ TITLE = config.get('export', 'title')
 PDF_FILE_NAME = config.get('export', 'pdf_file_name')
 PDF_TITLE = config.get('export', 'title')
 
-CLIENT_APP_FOLDER = os.path.join(os.environ['PWD'], config.get('client', 'app_folder'))
+CLIENT_APP_FOLDER = os.path.join(os.path.dirname(__file__), config.get('client', 'app_folder'))
 
 def send_mail(sender, recipient, subject, contents, attachments=None):
     """Sends an email to the provided recipient
